4a5357974e0f81d944e5f31721aecdbe2b1d0741
marcoyimyaem <marcoyimyaem@gmail.com>
2024-06-19 12:59:05 +0000

    day 6

--- Main.java
+++ Main.java
@@ -1,1 +1,32 @@
+ class Main{
+    public static void main(String[] args) {
+        Client c1 = new Client();
+        System.out.println("Hello World");
+    }
 
+}
+/*
+ * this
+ * is
+ * a 
+ * multiline
+ * comment
+ */
+/**
+ * Client object lorem ipsum...
+ * @author Marco Yimyaem
+ * @param not aplicable
+ */
+ class Client{
+    String Lname; // clients last name
+    String Fname; // clients first name
+    String Address;
+    int idNum;
+    String accType;
+}
+class Transactions{
+    // DateTime transDate;
+    String transType;
+    int amount;
+    int idNum;
+}
